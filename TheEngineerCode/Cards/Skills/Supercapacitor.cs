3951a23b5c6c130dc31726df2be955b80537c4fc
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-16 22:45:53 +0200

    Fixed super cap tool tip

--- TheEngineerCode/Cards/Skills/Supercapacitor.cs
+++ TheEngineerCode/Cards/Skills/Supercapacitor.cs
@@ -1,6 +1,7 @@
 ﻿using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheEngineer.TheEngineerCode.Cards;
@@ -14,6 +15,11 @@ public class Supercapacitor() : TheEngineerCard(1,
     CardType.Skill, CardRarity.Common,
     TargetType.Self)
 {
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        EngineerHoverTips.GetStaticHoverTip("THEENGINEER-CHARGE_MAX")
+    ];
     protected override IEnumerable<DynamicVar> CanonicalVars => [
         new BlockVar(6,ValueProp.Move)
     ];
