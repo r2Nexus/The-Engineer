ca7b776bf5fc49281a014cb533fba79c2db95ae8
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-17 21:03:57 +0200

    Fixed big drill loc + hover tip

--- TheEngineerCode/Cards/Attacks/BigDrill.cs
+++ TheEngineerCode/Cards/Attacks/BigDrill.cs
@@ -2,6 +2,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheEngineer.TheEngineerCode.Cards;
@@ -17,6 +18,12 @@ public class BigDrill() : TheEngineerCard(2,
 {
     private const decimal BASE_DAMAGE = 14;
     private const decimal UPGRADE_DAMAGE = 4;
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.Channeling),
+        HoverTipFactory.FromOrb<MinerOrb>()
+    ];
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [
         new DamageVar(BASE_DAMAGE, ValueProp.Move)
