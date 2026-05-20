63b44eac90f2ba0d14e0f0b00b049d71b36deef8
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-20 20:03:53 +0200

    Added Cliff Explosive card

---
+++ TheEngineerCode/Cards/Attacks/CliffExplosive.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheEngineer.TheEngineerCode.Character;
+
+namespace TheEngineer.TheEngineerCode.Cards.Attacks;
+
+
+[Pool(typeof(TheEngineerCardPool))]
+public class CliffExplosive() : TheEngineerCard(
+    1,
+    CardType.Attack,
+    CardRarity.Uncommon,
+    TargetType.AllEnemies)
+{
+    private const decimal BASE_DAMAGE = 7m;
+    private const decimal UPGRADE_DAMAGE = 2m;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(BASE_DAMAGE, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .TargetingAllOpponents(CombatState)
+            .WithAttackerAnim("Cast", Owner.Character.CastAnimDelay)
+            .WithHitFx("vfx/vfx_attack_blunt", tmpSfx: "heavy_attack.mp3")
+            .Execute(choiceContext);
+
+        CardSelectorPrefs prefs = new(SelectionScreenPrompt, 1)
+        {
+            PretendCardsCanBePlayed = true
+        };
+
+        CardModel? card = (await CardSelectCmd.FromHand(
+                choiceContext,
+                Owner,
+                prefs,
+                c => c != this,
+                this))
+            .FirstOrDefault();
+
+        if (card != null)
+        {
+            await CardCmd.Exhaust(choiceContext, card);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
+    }
+}
