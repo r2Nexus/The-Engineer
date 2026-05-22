860f45e67b7e725704855de94cd34086ba004568
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-22 20:13:11 +0200

    Reworked Rapid Depolyment

--- TheEngineerCode/Cards/Skills/RapidDeployment.cs
+++ TheEngineerCode/Cards/Skills/RapidDeployment.cs
@@ -1,47 +1,80 @@
-﻿using BaseLib.Utils;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Extensions;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using TheEngineer.TheEngineerCode.Cards;
 using TheEngineer.TheEngineerCode.Character;
 using TheEngineer.TheEngineerCode.Orbs;
+using TheEngineer.TheEngineerCode.Util;
 
 namespace TheEngineer.TheEngineerCode.Cards.Skills;
 
 [Pool(typeof(TheEngineerCardPool))]
-public class RapidDeployment() : TheEngineerCard(0,
-    CardType.Skill, CardRarity.Rare,
+public sealed class RapidDeployment() : TheEngineerCard(
+    1,
+    CardType.Skill,
+    CardRarity.Uncommon,
     TargetType.Self)
 {
-    protected override bool HasEnergyCostX => true;
-    
-    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    private const decimal BASE_CHARGE_INITIAL = 2m;
+    private const decimal BASE_CHARGE_MAX = 6m;
+
+    private const int BASE_CONSUME = 2;
+
+    protected override HashSet<CardTag> CanonicalTags =>
     [
-        HoverTipFactory.Static(StaticHoverTip.Channeling),
-        HoverTipFactory.FromOrb<TurretOrb>()
+        TheEngineerCardTags.Charge
     ];
+
+    protected override bool ShouldGlowGoldInternal =>
+        ChargeHelper.IsFull(this);
+
     public override IEnumerable<CardKeyword> CanonicalKeywords =>
+        IsUpgraded
+            ? []
+            : [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips
+    {
+        get
+        {
+            yield return HoverTipFactory.FromOrb<TurretOrb>();
+
+            if (!IsUpgraded)
+                yield return HoverTipFactory.FromKeyword(CardKeyword.Exhaust);
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        CardKeyword.Exhaust
+        new ConsumeVar(BASE_CONSUME),
+
+        new ChargeInitialVar(BASE_CHARGE_INITIAL),
+        new ChargeCurrentVar(BASE_CHARGE_INITIAL),
+        new ChargeMaxVar(BASE_CHARGE_MAX)
     ];
-    
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        var energy = ResolveEnergyXValue();
-        for(var e = 0; e < energy; e++)
-        {
-            await OrbCmd.Channel<TurretOrb>(choiceContext, this.Owner);
-        }
-    }
+        await OrbCmd.Channel<TurretOrb>(choiceContext, Owner);
 
-    protected override void OnUpgrade()
-    {
-        RemoveKeyword(CardKeyword.Exhaust);
+        bool consumed = await MaterialHelper.ConsumeMaterial(
+            this,
+            choiceContext,
+            (int)DynamicVars.Consume().BaseValue,
+            MaterialSource.Hand,
+            play: play);
+
+        if (consumed)
+            await OrbCmd.Channel<TurretOrb>(choiceContext, Owner);
+
+        if (await ChargeHelper.TrySpendFullCharge(choiceContext, this, this))
+            await OrbCmd.Channel<TurretOrb>(choiceContext, Owner);
     }
 }
