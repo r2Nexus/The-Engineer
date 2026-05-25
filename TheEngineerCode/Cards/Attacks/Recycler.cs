b969410044ba18358d5611b77ba6ba1117493d4d
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-25 20:12:06 +0200

    Added Recycler

---
+++ TheEngineerCode/Cards/Attacks/Recycler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheEngineer.TheEngineerCode.Character;
+
+namespace TheEngineer.TheEngineerCode.Cards.Attacks;
+
+[Pool(typeof(TheEngineerCardPool))]
+public class Recycler() : TheEngineerCard(
+    1,
+    CardType.Attack,
+    CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    private const decimal BASE_DAMAGE = 10m;
+    private const decimal UPGRADE_DAMAGE = 3m;
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<Material>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(BASE_DAMAGE, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target);
+
+        await CommonActions.CardAttack(this, play.Target)
+            .Execute(choiceContext);
+
+        foreach (CardPile pile in new[]
+                 {
+                     PileType.Hand.GetPile(Owner),
+                     PileType.Draw.GetPile(Owner),
+                     PileType.Discard.GetPile(Owner)
+                 })
+        {
+            foreach (CardModel card in pile.Cards)
+            {
+                if (card.Type != CardType.Status)
+                    continue;
+
+                if (card.Keywords.Contains(TheEngineerKeyWords.Material))
+                    continue;
+
+                card.AddKeyword(TheEngineerKeyWords.Material);
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
+    }
+}
