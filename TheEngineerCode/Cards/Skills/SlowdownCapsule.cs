ade05b5b8a4cb45c47bbb483e25e453cb8cb13bc
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-05-31 20:37:19 +0200

    Reduced consume amount on slowdown capsule to 2

--- TheEngineerCode/Cards/Skills/SlowdownCapsule.cs
+++ TheEngineerCode/Cards/Skills/SlowdownCapsule.cs
@@ -23,7 +23,7 @@ public class SlowdownCapsule() : TheEngineerCard(1,
     private const decimal BASE_SLOW = 1;
     private const decimal UPGRADE_CORRODE = 0;
 
-    private const int BASE_CONSUME = 3;
+    private const int BASE_CONSUME = 2;
     private const int UPGRADE_CONSUME = -1;
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
