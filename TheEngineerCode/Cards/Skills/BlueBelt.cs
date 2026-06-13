f622489ae5aafb0140e3095a2ac48c36d3eb682a
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-06-13 14:45:49 +0200

    Reworked blue belt

--- TheEngineerCode/Cards/Skills/BlueBelt.cs
+++ TheEngineerCode/Cards/Skills/BlueBelt.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -9,29 +10,43 @@ namespace TheEngineer.TheEngineerCode.Cards.Skills;
 
 public class BlueBelt : TheEngineerCard
 {
+    private const int BASE_CONSUME = 2;
     private const int BASE_REPLAY = 1;
     private const int UPGRADE_REPLAY = 1;
     
-    public BlueBelt() : base(0,
-        CardType.Skill, CardRarity.Rare,
+    public BlueBelt() : base(
+        0,
+        CardType.Skill,
+        CardRarity.Rare,
         TargetType.Self)
-
     {
         _baseReplayCount = BASE_REPLAY;
     }
     
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new ConsumeVar(1),
-        new CardsVar(2)
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new ConsumeVar(BASE_CONSUME)
     ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        bool consumed = await MaterialHelper.ConsumeMaterial(this, choiceContext, (int)DynamicVars.Consume().BaseValue,
-            MaterialSource.Hand, play);
-        if (consumed) await CommonActions.Draw(this,choiceContext);
+        bool consumed = await MaterialHelper.ConsumeMaterial(
+            this,
+            choiceContext,
+            (int)DynamicVars.Consume().BaseValue,
+            MaterialSource.Stock);
+
+        if (!consumed)
+            return;
+
+        await CardPileCmd.AutoPlayFromDrawPile(
+            choiceContext,
+            Owner,
+            1,
+            CardPilePosition.Top,
+            false);
     }
 
     protected override void OnUpgrade()
