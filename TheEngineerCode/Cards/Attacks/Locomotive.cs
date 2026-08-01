ef5d921c4c8ae770799f0c29e070bb92e6de868d
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-01 13:54:37 +0200

    Locomotive rework

--- TheEngineerCode/Cards/Attacks/Locomotive.cs
+++ TheEngineerCode/Cards/Attacks/Locomotive.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Abstracts;
 using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Combat.History.Entries;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Saves.Runs;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheEngineer.TheEngineerCode.Character;
 using TheEngineer.TheEngineerCode.Util;
@@ -22,8 +20,17 @@ namespace TheEngineer.TheEngineerCode.Cards.Attacks;
 [Pool(typeof(TheEngineerCardPool))]
 public sealed class Locomotive : TheEngineerCard
 {
-    private const decimal BASE_DAMAGE = 8m;
-    private const decimal UPGRADE_DAMAGE = 4m;
+    private const string INCREASE_KEY = "Increase";
+    private const string INCREASE_PER_WAGON_KEY = "IncreasePerWagon";
+
+    private const int BASE_DAMAGE = 12;
+    private const int UPGRADE_DAMAGE = 6;
+
+    private const decimal BASE_INCREASE_PER_WAGON = 2m;
+    private const decimal UPGRADE_INCREASE_PER_WAGON = 0m;
+
+    private int _currentDamage = BASE_DAMAGE;
+    private int _increasedDamage;
 
     public Locomotive() : base(
         2,
@@ -33,14 +40,50 @@ public sealed class Locomotive : TheEngineerCard
     {
     }
 
+    [SavedProperty]
+    public int CurrentDamage
+    {
+        get => _currentDamage;
+        set
+        {
+            AssertMutable();
+
+            _currentDamage = value;
+            DynamicVars.Damage.BaseValue = value;
+        }
+    }
+
+    [SavedProperty]
+    public int IncreasedDamage
+    {
+        get => _increasedDamage;
+        set
+        {
+            AssertMutable();
+            _increasedDamage = value;
+        }
+    }
+
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
         EngineerCycleHoverTips.ForTag(TheEngineerCardTags.Wagon)
     ];
-    
+
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DamageVar(BASE_DAMAGE, ValueProp.Move)
+        new DamageVar(CurrentDamage, ValueProp.Move),
+
+        new IntVar(
+            INCREASE_PER_WAGON_KEY,
+            BASE_INCREASE_PER_WAGON),
+        
+        new CalculationBaseVar(0m),
+        new CalculationExtraVar(1m),
+
+        new CalculatedVar(INCREASE_KEY)
+            .WithMultiplier((card, _) =>
+                CountWagonsInStock(card)
+                * card.DynamicVars[INCREASE_PER_WAGON_KEY].BaseValue)
     ];
 
     public override HashSet<CardKeyword> CanonicalKeywords =>
@@ -54,33 +97,62 @@ public sealed class Locomotive : TheEngineerCard
     {
         ArgumentNullException.ThrowIfNull(play.Target);
 
-        await CommonActions.CardAttack(this, play.Target)
-            .WithAttackerAnim("Cast", Owner.Character.CastAnimDelay)
+        int damageIncrease = decimal.ToInt32(
+            ((CalculatedVar)DynamicVars[INCREASE_KEY])
+            .Calculate(play.Target));
+
+        await CommonActions.CardAttack(this, play)
+            .WithAttackerAnim(
+                "Cast",
+                Owner.Character.CastAnimDelay)
             .Execute(choiceContext);
 
-        List<CardPlay> wagonPlays = CombatManager.Instance.History.CardPlaysFinished
-            .Where(e => e.Actor == Owner.Creature)
-            .Where(e => IsWagon(e.CardPlay.Card))
-            .Select(e => e.CardPlay)
-            .ToList();
+        BuffFromPlay(damageIncrease);
 
-        foreach (CardPlay wagonPlay in wagonPlays)
-        {
-            Creature? target = wagonPlay.Target;
-
-            if (target != null && !target.IsHittable)
-                target = null;
-
-            await CardCmd.AutoPlay(
-                choiceContext,
-                wagonPlay.Card.CreateDupe(Owner),
-                target);
-        }
+        if (DeckVersion is Locomotive deckVersion)
+            deckVersion.BuffFromPlay(damageIncrease);
     }
 
     protected override void OnUpgrade()
     {
+        //DynamicVars[INCREASE_PER_WAGON_KEY].UpgradeValueBy(UPGRADE_INCREASE_PER_WAGON);
         DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
+    }
+
+    protected override void AfterDowngraded()
+    {
+        UpdateDamage();
+    }
+
+    private void BuffFromPlay(int extraDamage)
+    {
+        if (extraDamage <= 0)
+            return;
+
+        IncreasedDamage += extraDamage;
+        UpdateDamage();
+    }
+
+    private void UpdateDamage()
+    {
+        CurrentDamage = BASE_DAMAGE + IncreasedDamage;
+    }
+
+    private static decimal CountWagonsInStock(CardModel card)
+    {
+        return CountWagons(card, PileType.Hand)
+             + CountWagons(card, PileType.Draw)
+             + CountWagons(card, PileType.Discard);
+    }
+
+    private static int CountWagons(
+        CardModel card,
+        PileType pileType)
+    {
+        return pileType
+            .GetPile(card.Owner)
+            .Cards
+            .Count(IsWagon);
     }
 
     private static bool IsWagon(CardModel card)
