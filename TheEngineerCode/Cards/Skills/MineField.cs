ae4d96c050f44463116e8622c3ccbfb2f0005e9b
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-01 15:41:23 +0200

    Reworked Mine Field

--- TheEngineerCode/Cards/Skills/MineField.cs
+++ TheEngineerCode/Cards/Skills/MineField.cs
@@ -1,30 +1,45 @@
-﻿using BaseLib.Utils;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Extensions;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using TheEngineer.TheEngineerCode.Cards;
+using MegaCrit.Sts2.Core.Models.Powers;
 using TheEngineer.TheEngineerCode.Character;
 using TheEngineer.TheEngineerCode.Orbs;
+using TheEngineer.TheEngineerCode.Powers;
 
 namespace TheEngineer.TheEngineerCode.Cards.Skills;
 
 [Pool(typeof(TheEngineerCardPool))]
-public class MineField() : TheEngineerCard(0,
-    CardType.Skill, CardRarity.Rare,
+public sealed class MineField() : TheEngineerCard(
+    2,
+    CardType.Skill,
+    CardRarity.Rare,
     TargetType.Self)
 {
-    protected override bool HasEnergyCostX => true;
-    
+    private const decimal BASE_ORB_SLOTS = 2m;
+    private const decimal UPGRADE_ORB_SLOTS = 1m;
+
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
         HoverTipFactory.Static(StaticHoverTip.Channeling),
         HoverTipFactory.FromOrb<LandMineOrb>()
     ];
-    
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
 
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<MineFieldPower>(BASE_ORB_SLOTS)
+    ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+    [
+        CardKeyword.Exhaust
+    ];
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
@@ -33,16 +48,30 @@ public class MineField() : TheEngineerCard(0,
             Owner.Creature,
             "Cast",
             Owner.Character.CastAnimDelay);
-        var energy = ResolveEnergyXValue();
-        for(var e = 0; e < energy; e++)
+
+        // This power must increase the capacity immediately
+        // and remove the same number of slots at end of turn.
+        await CommonActions.ApplySelf<MineFieldPower>(choiceContext,this);
+
+        if (Owner.PlayerCombatState != null)
         {
-            await OrbCmd.Channel<LandMineOrb>(choiceContext, this.Owner);
+            var orbQueue = Owner.PlayerCombatState.OrbQueue;
+
+            int emptySlots = Math.Max(
+                0,
+                orbQueue.Capacity - orbQueue.Orbs.Count);
+
+            for (int i = 0; i < emptySlots; i++)
+            {
+                await OrbCmd.Channel<LandMineOrb>(
+                    choiceContext,
+                    Owner);
+            }
         }
-        if(IsUpgraded) await OrbCmd.Channel<LandMineOrb>(choiceContext, this.Owner);
     }
 
     protected override void OnUpgrade()
     {
-
+        DynamicVars.Power<MineFieldPower>().UpgradeValueBy(UPGRADE_ORB_SLOTS);
     }
 }
---
+++ TheEngineerCode/Powers/MineFieldPower.cs
@@ -0,0 +1,35 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheEngineer.TheEngineerCode.Powers;
+
+public class MineFieldPower : TheEngineerPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier,
+        CardModel? cardSource)
+    {
+        if(power != this) return;
+        if (Owner.Player == null) return;
+        if(power.Owner != Owner) return;
+        
+        if (amount > 0 ) await OrbCmd.AddSlots(Owner.Player, (int)amount);
+        if (amount < 0 ) OrbCmd.RemoveSlots(Owner.Player, (int)-amount);
+    }
+    
+    public override async Task AfterPlayerTurnStart(
+        PlayerChoiceContext choiceContext,
+        Player player)
+    {
+        if (player != Owner.Player)
+            return;
+
+        await PowerCmd.ModifyAmount(choiceContext, this, -Amount, null, null);
+    }
+}
