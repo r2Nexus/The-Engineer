cd5f1d0ac8d732565dee2227a9f2588df6a28d1f
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-04 18:48:02 +0200

    buffed green science

--- TheEngineerCode/Cards/Powers/GreenScience.cs
+++ TheEngineerCode/Cards/Powers/GreenScience.cs
@@ -2,16 +2,19 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using TheEngineer.TheEngineerCode.Cards;
 using TheEngineer.TheEngineerCode.Character;
 using TheEngineer.TheEngineerCode.Powers;
+using TheEngineer.TheEngineerCode.Util;
 
 namespace TheEngineer.TheEngineerCode.Cards.Powers;
 
 
 [Pool(typeof(TheEngineerCardPool))]
-public class GreenScience() : TheEngineerCard(2,
+public class GreenScience() : TheEngineerCard(1,
     CardType.Power, CardRarity.Uncommon,
     TargetType.Self)
 {
@@ -19,19 +22,27 @@ public class GreenScience() : TheEngineerCard(2,
     
     protected override HashSet<CardTag> CanonicalTags => [TheEngineerCardTags.Science];
     
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new PowerVar<GreenSciencePower>(BASE_POWER)
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<GreenSciencePower>(BASE_POWER),
     ];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        IsUpgraded
+            ? [EngineerHoverTips.GetStaticHoverTip("THEENGINEER-PRODUCEALL")]
+            : [];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
         await CommonActions.ApplySelf<GreenSciencePower>(this,DynamicVars.Power<GreenSciencePower>().BaseValue);
+        if (IsUpgraded)
+            await MaterialHelper.ProduceMaterial((CardModel)this, choiceContext, 2,
+                MaterialDestination.Hand);
     }
 
     protected override void OnUpgrade()
     {
-        EnergyCost.UpgradeBy(-1);
     }
 }
