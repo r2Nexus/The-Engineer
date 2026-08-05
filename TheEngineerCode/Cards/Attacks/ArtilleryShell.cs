496820979530bf91da93fe0c637e35c90b82c2e5
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-05 18:27:07 +0200

    Artillery wagon rework

---
+++ TheEngineerCode/Cards/Attacks/ArtilleryShell.cs
@@ -0,0 +1,42 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheEngineer.TheEngineerCode.Cards;
+using TheEngineer.TheEngineerCode.Character;
+
+namespace TheEngineer.TheEngineerCode.Cards.Attacks;
+
+[Pool(typeof(TokenCardPool))]
+public class ArtilleryShell() : TheEngineerCard(0,
+    CardType.Attack, CardRarity.Token,
+    TargetType.AnyEnemy)
+{
+    private const decimal BASE_DAMAGE = 10m;
+    private const decimal UPGRADE_DAMAGE = 3m;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(BASE_DAMAGE, ValueProp.Move)
+    ];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+    [
+        CardKeyword.Exhaust,
+        TheEngineerKeyWords.Material
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play)
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
+    }
+}
--- TheEngineerCode/Cards/Attacks/ArtilleryWagon.cs
+++ TheEngineerCode/Cards/Attacks/ArtilleryWagon.cs
@@ -1,56 +1,80 @@
-﻿using BaseLib.Utils;
+﻿using System.Linq;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
-using TheEngineer.TheEngineerCode.Cards;
 using TheEngineer.TheEngineerCode.Character;
-using TheEngineer.TheEngineerCode.Util;
 
 namespace TheEngineer.TheEngineerCode.Cards.Attacks;
 
 [Pool(typeof(TheEngineerCardPool))]
-public class ArtilleryWagon : TheEngineerCard
+public class ArtilleryWagon() : TheEngineerCard(
+    2,
+    CardType.Attack,
+    CardRarity.Rare,
+    TargetType.AllEnemies)
 {
-    private const decimal BASE_DAMAGE = 18;
-    private const decimal UPGRADE_DAMAGE = 0;
+    private const decimal BASE_DAMAGE = 8m;
+    private const decimal UPGRADE_DAMAGE = 3m;
 
-    private const decimal BASE_CONSUME = 3;
-    private const decimal BASE_REPLAY = 2;
-    private const decimal UPPGRADE_REPLAY = 1;
+    protected override HashSet<CardTag> CanonicalTags =>
+    [
+        TheEngineerCardTags.Wagon
+    ];
 
-    public ArtilleryWagon() : base(1,
-        CardType.Attack, CardRarity.Rare,
-        TargetType.AllEnemies)
-    {
-        _baseReplayCount = (int)BASE_REPLAY;
-    }
-    
-    protected override HashSet<CardTag> CanonicalTags => [TheEngineerCardTags.Wagon];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(BASE_DAMAGE, ValueProp.Move)
+    ];
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
-        EngineerHoverTips.GetStaticHoverTip("THEENGINEER-STOCK")
-    ];
-    
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(BASE_DAMAGE,ValueProp.Move),
-        new ConsumeVar(BASE_CONSUME)
+        HoverTipFactory.FromCard<ArtilleryShell>()
     ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        bool consumed = await MaterialHelper.ConsumeMaterial(this, choiceContext, (int)DynamicVars.Consume().BaseValue,
-            MaterialSource.Stock, play);
-        if (consumed) await CommonActions.CardAttack(this, play).WithAttackerAnim("Cast", Owner.Character.CastAnimDelay).Execute(choiceContext);
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+
+        List<CardModel> materials = PileType.Hand
+            .GetPile(Owner)
+            .Cards
+            .Concat(PileType.Draw.GetPile(Owner).Cards)
+            .Where(card =>
+                card.IsTransformable &&
+                card.Keywords.Contains(TheEngineerKeyWords.Material))
+            .ToList();
+
+        List<CardTransformation> transformations = [];
+
+        foreach (CardModel material in materials)
+        {
+            if (CombatState != null)
+            {
+                CardModel shell =
+                    CombatState.CreateCard<ArtilleryShell>(Owner);
+
+                transformations.Add(
+                    new CardTransformation(material, shell));
+            }
+        }
+
+        if (transformations.Count > 0)
+        {
+            await CardCmd.Transform(
+                transformations,
+                null);
+        }
     }
 
     protected override void OnUpgrade()
     {
-        //DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
-        BaseReplayCount += (int)BASE_REPLAY;
+        DynamicVars.Damage.UpgradeValueBy(UPGRADE_DAMAGE);
     }
 }
