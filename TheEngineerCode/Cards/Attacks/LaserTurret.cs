33aa551a60064c8ea9abea5f421c7586309eb05e
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-05 22:21:12 +0200

    Laser turret rework

--- TheEngineerCode/Cards/Attacks/LaserTurret.cs
+++ TheEngineerCode/Cards/Attacks/LaserTurret.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Commands;
+﻿using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheEngineer.TheEngineerCode.Character;
-using TheEngineer.TheEngineerCode.Orbs;
 using TheEngineer.TheEngineerCode.Util;
 
 namespace TheEngineer.TheEngineerCode.Cards.Attacks;
@@ -22,27 +15,24 @@ public sealed class LaserTurret() : TheEngineerCard(
     CardRarity.Uncommon,
     TargetType.AnyEnemy)
 {
-    private const decimal BASE_DAMAGE = 4m;
+    private const decimal BASE_DAMAGE = 5m;
+
+    private const int BASE_HITS = 2;
+    private const int CHARGED_EXTRA_HITS = 2;
 
     private const decimal BASE_CHARGE_INITIAL = 2m;
     private const decimal UPGRADE_CHARGE_INITIAL = 0m;
-    
+
     private const decimal BASE_CHARGE_MAX = 8m;
     private const decimal UPGRADE_CHARGE_MAX = -2m;
 
-    private const int CHARGED_EXTRA_HITS = 2;
-
-    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
-    [
-        HoverTipFactory.FromOrb<TurretOrb>()
-    ];
-    
     protected override HashSet<CardTag> CanonicalTags =>
     [
         TheEngineerCardTags.Charge
     ];
-    
-    protected override bool ShouldGlowGoldInternal => ChargeHelper.IsFull(this);
+
+    protected override bool ShouldGlowGoldInternal =>
+        ChargeHelper.IsFull(this);
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
@@ -52,41 +42,43 @@ public sealed class LaserTurret() : TheEngineerCard(
         new ChargeCurrentVar(BASE_CHARGE_INITIAL),
         new ChargeMaxVar(BASE_CHARGE_MAX),
 
-        ..MakeCalculatedVar("CalculatedHits", 0,
-            (card, target) =>
-                TurretHelper.GetOrbs<TurretOrb>(card.Owner).Count
-                + (ChargeHelper.IsFull(card) ? CHARGED_EXTRA_HITS : 0))
+        ..MakeCalculatedVar(
+            "CalculatedHits",
+            BASE_HITS,
+            (card, _) =>
+                BASE_HITS +
+                (ChargeHelper.IsFull(card)
+                    ? CHARGED_EXTRA_HITS
+                    : 0))
     ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        ArgumentNullException.ThrowIfNull(play.Target);
+        bool wasFullyCharged = ChargeHelper.IsFull(this);
 
-        int hits = TurretHelper.GetOrbs<TurretOrb>(Owner).Count;
+        await CommonActions.CardAttack(this, play)
+            .Execute(choiceContext);
 
-        if (await ChargeHelper.TrySpendFullCharge(choiceContext, this, this))
-            hits += CHARGED_EXTRA_HITS;
-
-        if (hits <= 0)
-            return;
-
-        for (int i = 0; i < hits; i++)
+        if (wasFullyCharged)
         {
-            await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
-                .FromCard(this, play)
-                .Targeting(play.Target)
-                .WithAttackerAnim("Cast", Owner.Character.CastAnimDelay)
-                .WithHitFx("vfx/vfx_attack_blunt", tmpSfx: "heavy_attack.mp3")
-                .Execute(choiceContext);
+            await ChargeHelper.TrySpendFullCharge(
+                choiceContext,
+                this,
+                this);
         }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.ChargeInitial().UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
-        DynamicVars.ChargeCurrent().UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
-        DynamicVars.ChargeMax().UpgradeValueBy(UPGRADE_CHARGE_MAX);
+        DynamicVars.ChargeInitial()
+            .UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
+
+        DynamicVars.ChargeCurrent()
+            .UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
+
+        DynamicVars.ChargeMax()
+            .UpgradeValueBy(UPGRADE_CHARGE_MAX);
     }
 }
