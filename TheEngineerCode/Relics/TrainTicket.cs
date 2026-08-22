e28bf6ef6156e5267713d9d79d3296d157a335cb
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-22 11:49:54 +0200

    Added Train Ticket

---
+++ TheEngineerCode/Relics/TrainTicket.cs
@@ -0,0 +1,51 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using TheEngineer.TheEngineerCode.Character;
+
+namespace TheEngineer.TheEngineerCode.Relics;
+
+public sealed class TrainTicket : TheEngineerRelic
+{
+    private bool _triggeredThisTurn;
+
+    public override RelicRarity Rarity => RelicRarity.Uncommon;
+
+    public override async Task AfterCardPlayed(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        if (_triggeredThisTurn)
+            return;
+
+        CardModel card = cardPlay.Card;
+        
+        if (card.Owner != Owner)
+            return;
+
+        if (!card.Tags.Contains(TheEngineerCardTags.Wagon))
+            return;
+
+        _triggeredThisTurn = true;
+
+        Flash();
+
+        await CardPileCmd.Draw(
+            choiceContext,
+            1,
+            Owner);
+    }
+
+    public override Task AfterPlayerTurnStart(
+        PlayerChoiceContext choiceContext,
+        Player player)
+    {
+        if (player == Owner)
+            _triggeredThisTurn = false;
+
+        return Task.CompletedTask;
+    }
+}
