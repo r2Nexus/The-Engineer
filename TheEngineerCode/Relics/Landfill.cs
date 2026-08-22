9d09c060348e89781a87998e0521113ad695f3a0
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-22 11:39:44 +0200

    Landfill

---
+++ TheEngineerCode/Relics/Landfill.cs
@@ -0,0 +1,25 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using TheEngineer.TheEngineerCode.Character;
+
+namespace TheEngineer.TheEngineerCode.Relics;
+
+[Pool(typeof(TheEngineerRelicPool))]
+public class Landfill() : TheEngineerRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Common;
+
+    public override async Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState)
+    {
+        if (side != Owner.Creature.Side || combatState.RoundNumber > 1) return;
+        Flash();
+        await OrbCmd.AddSlots(Owner, 1);
+    }
+}
