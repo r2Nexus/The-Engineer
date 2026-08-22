64d88e7b8c1222f083c78925fae73688d31c0958
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-22 17:27:40 +0200

    Resource Buffer

---
+++ TheEngineerCode/Relics/ResourceBuffer.cs
@@ -0,0 +1,38 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheEngineer.TheEngineerCode.Character;
+using TheEngineer.TheEngineerCode.Relics;
+using TheEngineer.TheEngineerCode.Util;
+
+namespace TheEngineer.TheEngineerCode.Relics;
+
+[Pool(typeof(TheEngineerRelicPool))]
+public class ResourceBuffer() : TheEngineerRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Shop;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new ProduceVar(3)
+    ];
+
+    public override async Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState)
+    {
+        if (side != Owner.Creature.Side || combatState.RoundNumber > 1) return;
+        Flash();
+        await MaterialHelper.ProduceMaterial(
+            Owner,
+            choiceContext,
+            (int)DynamicVars.Produce().BaseValue,
+            MaterialDestination.Discard,
+            this);
+    }
+}
