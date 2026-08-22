00717218a03f4438beff0dd1986d7880e59556da
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-08-22 13:14:58 +0200

    Logi bot

---
+++ TheEngineerCode/Relics/LogisticsBot.cs
@@ -0,0 +1,40 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheEngineer.TheEngineerCode.Relics;
+
+public sealed class LogisticsBot : TheEngineerRelic
+{
+    public override RelicRarity Rarity => RelicRarity.Uncommon;
+
+    public override Task BeforeFlush(
+        PlayerChoiceContext choiceContext,
+        Player player)
+    {
+        if (player != Owner)
+            return Task.CompletedTask;
+
+        List<CardModel> skills = PileType.Hand
+            .GetPile(player)
+            .Cards
+            .Where(card =>
+                card.Type == CardType.Skill &&
+                !card.ShouldRetainThisTurn)
+            .ToList();
+
+        if (skills.Count == 0)
+            return Task.CompletedTask;
+
+        CardModel? selected =
+            Owner.RunState.Rng.CombatCardSelection.NextItem(skills);
+
+        selected.GiveSingleTurnRetain();
+
+        Flash();
+
+        return Task.CompletedTask;
+    }
+}
