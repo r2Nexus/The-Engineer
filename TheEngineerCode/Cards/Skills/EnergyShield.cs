1a71e791c1a4e80e9b11c5f9fc0c7cdde77ad645
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-09-24 18:56:41 +0200

    E shield is now affected by powers properly

--- TheEngineerCode/Cards/Skills/EnergyShield.cs
+++ TheEngineerCode/Cards/Skills/EnergyShield.cs
@@ -20,30 +20,22 @@ public sealed class EnergyShield() : TheEngineerCard(
     TargetType.Self)
 {
     private const decimal BASE_BLOCK = 8m;
-    private const decimal UPGRADE_BLOCK = 0m;
-
-    private const decimal BASE_CHARGED_BLOCK = 8m;
-    private const decimal UPGRADE_CHARGED_BLOCK = 0m;
 
     private const decimal BASE_CHARGE_INITIAL = 3m;
-    private const decimal UPGRADE_CHARGE_INITIAL = 0m;
 
     private const decimal BASE_CHARGE_MAX = 8m;
     private const decimal UPGRADE_CHARGE_MAX = -2m;
 
-    private const string CHARGED_BLOCK_VAR = "ChargedBlock";
-
     protected override HashSet<CardTag> CanonicalTags => [TheEngineerCardTags.Charge];
     protected override bool ShouldGlowGoldInternal => ChargeHelper.IsFull(this);
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
-    {
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
         new BlockVar(BASE_BLOCK, ValueProp.Move),
         new ChargeInitialVar(BASE_CHARGE_INITIAL),
         new ChargeCurrentVar(BASE_CHARGE_INITIAL),
-        new ChargeMaxVar(BASE_CHARGE_MAX),
-        new IntVar(CHARGED_BLOCK_VAR, BASE_CHARGED_BLOCK)
-    };
+        new ChargeMaxVar(BASE_CHARGE_MAX)
+    ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
@@ -61,20 +53,12 @@ public sealed class EnergyShield() : TheEngineerCard(
 
         if (await ChargeHelper.TrySpendFullCharge(choiceContext, this, this))
         {
-            await CreatureCmd.GainBlock(
-                Owner.Creature,
-                DynamicVars[CHARGED_BLOCK_VAR].BaseValue,
-                ValueProp.Unpowered,
-                null);
+            await CommonActions.CardBlock(this, cardPlay);
         }
     }
 
     protected override void OnUpgrade()
     {
-        //DynamicVars.Block.UpgradeValueBy(UPGRADE_BLOCK);
-        //DynamicVars[CHARGED_BLOCK_VAR].UpgradeValueBy(UPGRADE_CHARGED_BLOCK);
-        DynamicVars.ChargeInitial().UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
-        DynamicVars.ChargeCurrent().UpgradeValueBy(UPGRADE_CHARGE_INITIAL);
         DynamicVars.ChargeMax().UpgradeValueBy(UPGRADE_CHARGE_MAX);
     }
 }
